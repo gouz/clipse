a60b083bb3b1b5f128fe8cfb0ada6e4bb77a5a03
gouz <sylvain.gougouzian@gmail.com>
2025-05-12 14:45:48 +0200

    feat: build 0.0.11

--- dist/index.cjs
+++ dist/index.cjs
@@ -94,11 +94,20 @@ Usage: ${this.#parent}${this.#name} [options] [arguments]
 ${subs}
 ` : "";
   }
+  #getVerboseOption(o) {
+    const [k, v] = Object.entries(o).at(0) ?? [];
+    if (k && v) {
+      const short = typeof v.short !== "undefined" ? `-${v.short}, ` : "";
+      const param = v.type === "boolean" ? "" : " <param>";
+      return `${short}--${k}${param}`;
+    }
+    return "";
+  }
   #helpOptions() {
     const options = Object.entries(this.#options).filter(([_, v]) => typeof v.long === "undefined");
-    const maxLength = Math.max(...options.map(([k, v]) => `${typeof v.short !== "undefined" ? `-${v.short}, ` : ""}--${k}`.length)) + 2;
+    const maxLength = Math.max(...options.map(([k, v]) => this.#getVerboseOption({ [k]: v }).length)) + 1;
     const opts = options.map(([k, v]) => [
-      `  \x1B[1m${`${typeof v.short !== "undefined" ? `-${v.short}, ` : ""}--${k}`.padEnd(maxLength)}\x1B[0m`,
+      `  \x1B[1m${this.#getVerboseOption({ [k]: v }).padEnd(maxLength)}\x1B[0m`,
       this.#helpDesc(v.description ?? ""),
       ` ${typeof v.default !== "undefined" ? `(default: ${v.default})` : ""}`,
       `
@@ -111,7 +120,7 @@ ${opts}
   #helpArguments() {
     let args = "";
     if (this.#arguments.length) {
-      const maxLength = Math.max(...this.#arguments.map((a) => a.name.length)) + 2;
+      const maxLength = Math.max(...this.#arguments.map((a) => a.name.length)) + 1;
       args = this.#arguments.map((a) => `  \x1B[1m${a.name.padEnd(maxLength)}\x1B[0m ${this.#helpDesc(a.description ?? "")}
 `).join("");
     }
@@ -148,49 +157,61 @@ ${args}
     this.#action = a;
     return this;
   }
-  #parseOptions(argv) {
-    const options = {};
-    argv.forEach((ar, i) => {
-      if (/^-[a-z]+$/.exec(ar)) {
-        const shorts = ar.substring(1).split("");
-        shorts.forEach((s, j) => {
-          if (this.#options[s]?.type !== "boolean") {
-            if (j === shorts.length - 1) {
-              if (!argv[i + 1]?.startsWith("-")) {
-                options[this.#options[s]?.long ?? ""] = argv[i + 1] ?? this.#options[s]?.default;
-                argv.splice(i + 1, 1);
-              }
+  #parseShortOptions(argv, ar, options) {
+    if (ar.includes("=")) {
+      const [k, v] = ar.substring(1).split("=", 2);
+      options[this.#options[k]?.long ?? ""] = v;
+    } else {
+      const shorts = ar.substring(1).split("");
+      shorts.forEach((s, j) => {
+        if (this.#options[s]?.type !== "boolean") {
+          if (j === shorts.length - 1) {
+            if (!argv[1]?.startsWith("-")) {
+              options[this.#options[s]?.long ?? ""] = argv[1] ?? this.#options[s]?.default;
+              argv.shift();
             } else {
               options[this.#options[s]?.long ?? ""] = this.#options[s]?.default;
             }
           } else {
-            options[this.#options[s].long ?? ""] = true;
-          }
-        });
-        argv.splice(i, 1);
-      } else if (ar.startsWith("--")) {
-        const a = ar.substring(2);
-        if (a.includes("=")) {
-          const [k, v] = a.split("=", 2);
-          options[k] = v;
-        } else if (this.#options[a]?.type !== "boolean") {
-          if (i === argv.length - 1) {
-            options[a] = this.#options[a]?.default;
-          } else {
-            options[a] = argv[i + 1];
-            argv.splice(i + 1, 1);
+            options[this.#options[s]?.long ?? ""] = this.#options[s]?.default;
           }
         } else {
-          options[a] = true;
+          options[this.#options[s].long ?? ""] = true;
         }
-        argv.splice(i, 1);
-      } else if (ar.startsWith("-") && ar.includes("=")) {
-        const [k, v] = ar.substring(1).split("=", 2);
-        options[this.#options[k]?.long ?? ""] = v;
-        argv.splice(i, 1);
+      });
+    }
+    argv.shift();
+  }
+  #parseLongOptions(argv, ar, options) {
+    const a = ar.substring(2);
+    if (a.includes("=")) {
+      const [k, v] = a.split("=", 2);
+      options[k] = v;
+    } else if (this.#options[a]?.type !== "boolean") {
+      if (argv.length === 1) {
+        options[a] = this.#options[a]?.default;
+      } else {
+        options[a] = argv[1];
+        argv.shift();
       }
-    });
-    return options;
+    } else {
+      options[a] = true;
+    }
+    argv.shift();
+  }
+  #parseOptions(argv) {
+    const options = {};
+    const args = [];
+    while (argv.length) {
+      const ar = argv[0] ?? "";
+      if (/^-[a-z=]+$/.exec(ar))
+        this.#parseShortOptions(argv, ar, options);
+      else if (ar.startsWith("--"))
+        this.#parseLongOptions(argv, ar, options);
+      else
+        args.push(argv.shift() ?? "");
+    }
+    return { options, args };
   }
   #parseArguments(argv) {
     const args = {};
@@ -206,8 +227,8 @@ ${args}
       argv.push(...process.argv.slice(2));
     const options = {};
     Object.entries(this.#options).forEach(([key, value], _) => {
-      if (value.optional !== true && typeof value.default !== "undefined" && typeof value.long === "undefined")
-        options[key] = value.default;
+      if (typeof value.long === "undefined" && (typeof value.optional === "undefined" || !value.optional) && !["help", "version"].includes(key))
+        options[key] = value.default ?? (value.type === "boolean" ? false : "");
     });
     if (argv.length) {
       if (argv[0] === "-h" || argv[0] === "--help") {
@@ -223,11 +244,12 @@ ${args}
         argv.shift();
         sub.ready(argv, `${this.#parent}${this.#name} `);
       } else {
+        const parsedOptions = this.#parseOptions([...argv]);
         const opts = {
           ...options,
-          ...this.#parseOptions(argv)
+          ...parsedOptions.options
         };
-        const args = this.#parseArguments(argv);
+        const args = this.#parseArguments([...parsedOptions.args]);
         await this.#action(args, opts);
       }
     } else {
